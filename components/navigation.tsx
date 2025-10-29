97129b9d2f87c321700afc508341769686ee91e4
ehdnd_mac <taeungjang@naver.com>
2025-10-29 15:23:12 +0900

    2 4 hydration

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,11 +2,14 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export default function Navigation() {
   // Client Component 에서만 사용가능
   const path = usePathname();
   console.log(path);
+
+  const [count, setCount] = useState(0);
 
   return (
     <nav>
@@ -18,7 +21,19 @@ export default function Navigation() {
           <Link href="/about-us">About Us</Link>
           {path === "/about-us" ? "(here)" : ""}
         </li>
+        <li>
+          <button onClick={() => setCount((c) => c + 1)}>{count}</button>
+        </li>
       </ul>
     </nav>
   );
 }
+
+/*
+/about-us ---> dummy HTML no interacttion (이벤트리스너 부착안됨)
+          ---> init() new React App with dummy HTML starting point
+          ---> navigation 이 a 에서 react component가 된다
+/about-us ---> <button>0</button>
+          ---> <button onClinck=...> 
+          ---> 이게 hydration 단순 HTML을 React App 으로 초기화하는 작업
+*/
