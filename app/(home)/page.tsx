6f97afc373fce1bb0ccbd3e6211d8e6a79e0ce87
ehdnd_mac <taeungjang@naver.com>
2025-10-30 17:37:39 +0900

    3 2 server side

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,38 +1,33 @@
-"use client";
+import { Metadata } from "next";
+import { resolve } from "path";
 
-import { useEffect, useState } from "react";
+export const metadata: Metadata = {
+  title: "Home",
+};
 
-// 먼저 react 에서 기본적으로 사용하던 fetch 방시을 사용해보자
-// client 브라우저에서 api에 요청을 보낸다
-// api key를 사용할 수도 없고 DB와 소통할 수도 없다
-// 사용자는 항상 loading을 봐야한다
+const URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
-// 그래서 주로 보통은 ...
-// React App <-> API <-> DB
+// server component라 Page() 안에 없어도 된다
+// backend 에서 영화를 fetch 해온다
+async function getMovies() {
+  // 이렇게 되면 이 작업이 완료되기 전까지 사용자는 '아무것도' 보지 못한다
+  // loading state가 이동되었기 때문이다
+  await new Promise((resolve) => setTimeout(resolve, 5000));
 
-export default function Page() {
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  // fetch한 데이터를 알아서 캐싱해둔다
+  // 첫 번째 fetch 만 요청하고 이후 요청부터는 api 호출하지 않는다
+  // 최신데이터를 가져와야하는 경우는 나중에 다루자.
+  return await (await fetch(URL)).json();
+}
 
-  const getMoives = async () => {
-    const json = await (
-      await fetch("https://nomad-movies.nomadcoders.workers.dev/movies")
-    ).json();
-
-    setMovies(json);
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    getMoives();
-  }, []);
-
-  console.log(movies);
+// async function 으로 바꿔주자
+export default async function HomePage() {
+  const movies = await getMovies();
 
   return (
     <div>
       <h1>Hello NextJs!</h1>
-      <div>{isLoading ? "loading..." : JSON.stringify(movies)}</div>
+      <div>{JSON.stringify(movies)}</div>
     </div>
   );
 }
